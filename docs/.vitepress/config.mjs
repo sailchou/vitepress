f04ebb5450bbc5ec61312f45d84c2d85efca01b4
sailchou <6948982+sailchou@users.noreply.github.com>
2024-09-23 16:29:51 +0800

    base

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: 'vitepress',
   title: "SAILCHOU的博客",
   description: "A VitePress Site",
   themeConfig: {
