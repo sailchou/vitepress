33d149c8822b02643b2d99f5f694efb87226c579
sailchou <6948982+sailchou@users.noreply.github.com>
2024-09-23 15:52:02 +0800

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "SAILCHOU的博客",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '技术总结', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          {
+            text: 'SSR Examples', items: [
+              { text: 'Markdown Examples', link: '/ssr-markdown-examples' },
+              { text: 'Runtime API Examples', link: '/ssr-api-examples' },
+            ]
+          }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/sailchou' }
+    ],
+
+    footer: {
+      // message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+      copyright: 'Copyright © 2024-present <a href="https://github.com/sailchou">sailchou</a>'
+    },
+
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+
+    search: {
+      provider: 'local'
+    }
+
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+    name: '项目技术总结'
+    text: 'A VitePress Site'
+    tagline: Next.js,React,Node.js
+    actions:
+        - theme: brand
+          text: 开始阅读
+          link: /markdown-examples
+        - theme: alt
+          text: API Examples
+          link: /api-examples
+
+features:
+    - title: Feature A
+      details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    - title: Feature B
+      details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    - title: Feature C
+      details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
